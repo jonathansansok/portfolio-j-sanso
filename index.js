d82d306c874c08b51a111c5e1618c32d95867574
¨jonathansansok¨ <jonasans2@live.com.ar>
2023-01-09 02:19:57 -0300

    ¨colors¨

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 //Nav selected function
 function selected(link) {
-  let options = document.querySelectorAll('#links a');
-  options[0].className = '';
-  options[1].className = '';
-  options[2].className = '';
-  options[3].className = '';
-  options[4].className = '';
-  link.className = 'selected';
+  let options = document.querySelectorAll("#links a");
+  options[0].className = "";
+  options[1].className = "";
+  options[2].className = "";
+  options[3].className = "";
+  options[4].className = "";
+  link.className = "selected";
 
-  let x = document.getElementById('nav');
-  x.className = ''
+  let x = document.getElementById("nav");
+  x.className = "";
 }
 
 //Shows the responsive menu
